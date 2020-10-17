bdf090669519674a12415e412466c2364b0f3542
Batashov Pavel <47531675+Paykan111@users.noreply.github.com>
2020-10-17 20:23:50 +0300

    Поменял ретёрн в сервисе

--- src/main/java/jspb/demo/service/UserServiceImp.java
+++ src/main/java/jspb/demo/service/UserServiceImp.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Service
 public class UserServiceImp implements UserService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
     public UserServiceImp(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
@@ -22,12 +22,11 @@ public class UserServiceImp implements UserService {
     }
 
     public List<DataDTO> dataForTable() {
-        List<DataDTO> dataDTOList = List.of(new DataDTO("Frozen Yogurt", 159, 6, 24, 4.0, "1%"),
+        return List.of(new DataDTO("Frozen Yogurt", 159, 6, 24, 4.0, "1%"),
                 new DataDTO("Ice cream sandwich", 237, 9.0, 37, 4.3, "1%"),
                 new DataDTO("Eclair", 262, 16.0, 23, 6.0, "7%"),
                 new DataDTO("Cupcake", 305, 3.7, 67, 4.3, "8%"),
                 new DataDTO("Gingerbread", 356, 16.0, 49, 3.9, "16%"),
                 new DataDTO("Jelly bean", 375, 0.0, 94, 0.0, "0%"));
-        return dataDTOList;
     }
 }
